4be90222e2a590336a8543a5773dd83c738d9974
tomot813 <tomot813tom@gmail.com>
2024-12-08 23:48:41 +0300

    first commit

---
+++ js/profile.js
@@ -0,0 +1,28 @@
+
+
+
+let login = sessionStorage.getItem("login");
+let date = sessionStorage.getItem("date");
+let sex = sessionStorage.getItem("sex");
+
+
+if(!login || !date){
+    window.location.href = "../html/singIn.html"
+
+}else{
+    document.getElementById("username").innerText = login;
+    document.getElementById("username2").innerText = login;
+    document.getElementById("date").innerText = date;
+    document.getElementById("sex").innerText = sex;
+}
+
+
+function clearDate(){
+    sessionStorage.removeItem("login"); //localStorage || sessionStorage
+    sessionStorage.removeItem("date"); //localStorage || sessionStorage
+
+    window.location.href = "../html/singIn.html"
+}
+
+document.getElementById("logout").addEventListener('click', clearDate);
+
