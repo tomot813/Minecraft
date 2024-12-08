528d474f4f08fd7b0931ebbf7b14afb8d26270b5
tomot813 <tomot813tom@gmail.com>
2024-12-09 00:27:59 +0300

    test

--- js/profile.js
+++ js/profile.js
@@ -7,7 +7,7 @@ let sex = sessionStorage.getItem("sex");
 
 
 if(!login || !date){
-    window.location.href = "../html/singin.html"
+    window.location.href = "./html/singin.html"
     alert("s")
 
 }else{
@@ -22,7 +22,7 @@ function clearDate(){
     sessionStorage.removeItem("login"); //localStorage || sessionStorage
     sessionStorage.removeItem("date"); //localStorage || sessionStorage
 
-    window.location.href = "../html/singIn.html"
+    window.location.href = "./html/singIn.html"
 }
 
 document.getElementById("logout").addEventListener('click', clearDate);
